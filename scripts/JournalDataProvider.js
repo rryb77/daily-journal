2f4f1ccd4e85aa48997466bf77d5eddf97e329d7
rryb77 <rryb77@gmail.com>
2020-12-12 09:57:56 -0600

    Added the ability to save/view entries via JSON DB

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,3 +1,4 @@
+const eventHub = document.querySelector('#container')
 //Setup the journal array
 let journal = []
 
@@ -14,3 +15,21 @@ export const getEntries = () => {
 
 // Make use of the data that was collected by exporting it around where needed
 export const useJournalEntries = () => journal.slice()
+
+// Broadcast that the app state has changed because a new item was added to the array
+export const dispatchStateChangeEvent = () => {
+    eventHub.dispatchEvent(new CustomEvent("journalStateChanged"))
+}
+
+export const saveJournalEntry = (newJournalEntry) => {
+    // Use `fetch` with the POST method to add your entry to your API
+    fetch("http://localhost:8088/entries", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newJournalEntry)
+    })
+        .then(getEntries)  // <-- Get all journal entries
+        .then(dispatchStateChangeEvent)  // <-- Broadcast the state change event
+}
